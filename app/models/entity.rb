787ef314c06fec62e2fcdeaeca0a42ecb9a6fb52
biruk-bereka <birukbeletech1212@gmail.com>
2023-06-01 19:12:30 +0300

    Add validation for models

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,4 +2,7 @@ class Entity < ApplicationRecord
   belongs_to :user
   has_many :entity_groups, dependent: :destroy
   has_many :groups, through: :entity_groups
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validate :amount, presence: true, comparison: { greater_than: 0 }
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :entity_groups, dependent: :destroy
   has_many :entities, through: :entity_groups, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :icon, presence: true
 end
