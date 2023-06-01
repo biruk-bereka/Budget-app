1c7a8c7c787c8a2355172254a3de69ac01344282
biruk-bereka <birukbeletech1212@gmail.com>
2023-06-01 17:50:24 +0300

    Add transaction page and it's funcitonality

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,4 +1,5 @@
 class Entity < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :groups
+  has_many :entity_groups, dependent: :destroy
+  has_many :groups, through: :entity_groups
 end
